39a152238e1c39f7721e4b7da751a223baa2f994
TMcFadden <tmac727@yahoo.com>
2018-08-24 17:00:45 -0400

    created pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get "/pages/:pages" => "pages#show"
+
   get '/events' => 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
