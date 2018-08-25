06f66aad8c102d8f2d0a01bfe13bfcd04d29adab
Robert B Garcia <robgarcia552@gmail.com>
2018-08-25 15:17:40 -0400

    drop users and userprofile table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
   has_one :user_profile
-
   has_many :events
-  
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
 end
