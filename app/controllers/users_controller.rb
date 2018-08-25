fa6520042eab98a2f9b4800c86ecb15c28da55e9
Robert B Garcia <robgarcia552@gmail.com>
2018-08-25 16:35:54 -0400

    save the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,21 @@
 class UsersController < ApplicationController
-
+  
     def new
+      @user = User.new
     end
-
+  
     def create
-        user = User.new(user_params)
-            if user.save
-                session[:user_id] = user.id
-                redirect_to '/users'
-            else
-                redirect_to '/signup'
-            end
+      @user = User.new(user_params)
+  
+      return render action: 'new' unless @user.save
+  
+      redirect_to root_path, notice: 'Created user'
     end
-
+  
     private
-
     def user_params
-        params.
-            require(:user).
-                permit(:first_name, 
-                       :last_name, 
-                       :password, 
-                       :password_confirmation)
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end
-
-end
+  
+  end
+  
