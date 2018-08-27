de843bd066e1855faf660ba5038f35f4e27ce592
Robert B Garcia <robgarcia552@gmail.com>
2018-08-27 10:56:19 -0400

    fix current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find_by(params[:id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def require_logged_in
