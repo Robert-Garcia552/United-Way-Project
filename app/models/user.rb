77af3a6fb8d9af0e96e943d0a7a452ffe4eff431
Robert B Garcia <robgarcia552@gmail.com>
2018-08-29 11:53:46 -0400

    New user receives email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,20 @@
 class User < ApplicationRecord
     validates :email, presence: true
 
-    after_create_commit :welcome_all_users
+    after_create_commit :send_welcome_email
     
     has_secure_password
 
     has_one :user_profile
 
     has_many :events
-
     has_many :rsvps
     has_many :attending_events, through: :rsvps, source: :event
    
     private
 
-    def welcome_all_users
-        User.where.not(id: self.id).each do |user|
-            welcome(user)
-        end
-    end
-
-    def welcome(user)
-        WelcomeMailer.with(user: user).welcome.deliver_later
-        sleep 1
+    def send_welcome_email
+        WelcomeMailer.with(user: self).welcome.deliver_later
     end
 
 end
