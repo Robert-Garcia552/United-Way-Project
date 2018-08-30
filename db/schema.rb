861c9fd1a6bfcfae1eb7ef92991ada6128a2a1cf
Robert B Garcia <robgarcia552@gmail.com>
2018-08-30 14:10:18 -0400

    add admin to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_234320) do
+ActiveRecord::Schema.define(version: 2018_08_30_180855) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_234320) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
