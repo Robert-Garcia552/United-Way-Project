914d3253b85ce2a6a5c553497bbf2779482c04ae
TMcFadden <tmac727@yahoo.com>
2018-09-02 12:44:28 -0400

    add share buttons, links, style, etc to ways to give page

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_01_164101) do
-ActiveRecord::Schema.define(version: 2018_08_30_195722) do
+ActiveRecord::Schema.define(version: 2018_09_01_165514) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -131,5 +130,4 @@ ActiveRecord::Schema.define(version: 2018_08_30_195722) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-end
 end
