d08d6ecaf3a066e8e285d50da58230a6bd0ab052
Robert B Garcia <robgarcia552@gmail.com>
2018-09-07 15:26:13 -0400

    work on reminder three days email

--- app/mailers/reminder_email_mailer.rb
+++ app/mailers/reminder_email_mailer.rb
@@ -1,6 +1,13 @@
 class ReminderEmailMailer < ApplicationMailer
+  
   def event_reminder_email
     @user = params[:user]
     mail(to: @user.email, subject: "REMINDER")
   end
+
+  def upcoming_event
+    @user = params[:user]
+    mail(to: @user.email, subject: "Your Event is 3 Days Away!")
+  end
+  
 end
