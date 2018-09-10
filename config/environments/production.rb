4af9d9400b4131821fa694cb14875fe1094ac5c4
Kristina Rivero <kristinarivero@gmail.com>
2018-09-10 15:46:58 -0400

    add host to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "United_Way_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'united-way-connect.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
